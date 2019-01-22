91c94c2597e43da2b97d80f50586cf5456da09d2
Seth Williams <swilliams9334@gmail.com>
2019-01-22 16:18:10 -0500

    No idea what happened, but stylesheet is now connected.

--- app.py
+++ app.py
@@ -20,6 +20,11 @@ def index():
     # 3. Open the csv file and read it using the CSV library. This will give you a list of rows.
     # See: https://docs.python.org/3/library/csv.html#csv.DictReader
 
+    #With this, I'm saying that I want to open the data.csv file as a csv file
+    #Then I'm making a varaiable called data_csv that uses a dictionary reader to read the csvfile/data.csv
+    # Then, for each (new var) 'row' in data_csv, I want to print the row called "image", row called
+    # category, row called title, etc.
+
     with open('data.csv') as csvfile:
         data_csv = csv.DictReader(csvfile)
         for row in data_csv:
@@ -28,7 +33,13 @@ def index():
     # 4. Loop over each row in the CSV. Each row is a blog post.
 
     # 5. Take post_html and replace {{title}} {{body}} {{author}} with the data in each blog post csv row
-
+    #Here I am making a new variable called columns. This is all a continuation of the function above
+    # I'm using the keys method to say that columns is equal to a row of dict keys in the csv file
+        # Then, I'm making a variable called current_post that is equal to post_html (see line 13)
+            #then, for each col (new var) in columns -row.keys-, I want to take current_post, which is post_html,
+            # and do the replace method, current_post.replace to replace the thing in post.html
+            # that is in {{}} with row[col] - row[col] is defined as going through each column,
+            #
             columns = row.keys()
             current_post = post_html
             for col in columns:
